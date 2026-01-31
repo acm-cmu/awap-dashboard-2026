7717524700a8e4e2d2f167458521da9a0dfb1f69
Matthew Milunic <62996888+mmilunicmobile@users.noreply.github.com>
2026-01-31 12:04:04 -0500

    Added Discord and GH link

--- src/pages/getting_started.tsx
+++ src/pages/getting_started.tsx
@@ -49,6 +49,24 @@ const GettingStarted: NextPage = () => (
             www.acmatcmu.com/awap-viewer-2026/
           </a>
           <br />
+          Engine GitHub Repo:{' '}
+          <a
+            href="https://github.com/acm-cmu/awap-game-engine-2026-public"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github.com/acm-cmu/awap-game-engine-2026-public
+            </a>
+          <br />
+          Discord Link:{' '}
+          <a
+            href="https://discord.gg/G7k63AYd"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            discord.gg/G7k63AYd
+            </a>
+          <br />
         </Card.Text>
       </Card.Body>
     </Card>
